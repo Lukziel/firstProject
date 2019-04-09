a5bb87e5d1c9b8f980781133e59169a926c5f007
lukziel <luk.ziel@gmail.com>
2019-04-09 22:35:44 +0200

    Add Challenging DOM test

---
+++ src/main/java/ChallengingDom.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class ChallengingDom {
+    WebDriver driver;
+
+
+    public ChallengingDom(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+
+}
