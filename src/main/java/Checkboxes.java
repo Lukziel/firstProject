2e7ac2c1197e918480cf8dc524fe9a51306d3be7
lukziel <luk.ziel@gmail.com>
2019-06-30 22:51:41 +0200

    Add Checkbox test

--- src/main/java/Checkboxes.java
+++ src/main/java/Checkboxes.java
@@ -4,14 +4,23 @@ import org.openqa.selenium.support.PageFactory;
 
 public class Checkboxes {
     WebDriver driver;
+    boolean isChecked;
+    boolean isUnChecked;
 
     public Checkboxes(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
 
-    public void CheckedCheckBox(){
-       driver.findElements(By.cssSelector("input:checked[type='checkbox']"));
+    public void UnCheckedCheckBox() {
+        isUnChecked = driver.findElement(By.xpath("//input[1]")).isSelected();
+        System.out.println(isUnChecked);
+
+    }
+    public void CheckedCheckBox() {
+        isChecked = driver.findElement(By.xpath("//input[2]")).isSelected();
+//      isChecked = driver.findElement(By.cssSelector("input:checked[type='checkbox']")).isSelected(); by CSS Selector
+        System.out.println(isChecked);
 
     }
 
--- src/test/java/TestCheckboxes.java
+++ src/test/java/TestCheckboxes.java
@@ -1,9 +1,19 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestCheckboxes extends TestBase {
     @Test
-    public void shouldCheckTheSelectionCheckbox(){
+    public void shouldCheckTheCheckedCheckbox(){
         mainPage.goToChackboxesLink();
         checkboxes.CheckedCheckBox();
+        Assert.assertTrue(checkboxes.isChecked);
     }
+
+    @Test
+    public void shouldCheckTheUnCheckedCheckbox(){
+        mainPage.goToChackboxesLink();
+        checkboxes.UnCheckedCheckBox();
+        Assert.assertFalse(checkboxes.isUnChecked);
+    }
+    
 }
